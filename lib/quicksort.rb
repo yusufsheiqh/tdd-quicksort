afc9ac2a24e72b7fc76e3481fe0b967ed048064a
Yusuf <yusuf@madetech.com>
2019-03-29 15:11:45 +0000

    Passing fourth test - refactor

--- lib/quicksort.rb
+++ lib/quicksort.rb
@@ -3,9 +3,9 @@
 class QuickSort
   def self.sort(unsorted)
     if unsorted.length > 1 && unsorted[0] > unsorted[1]
-      unsorted[0], unsorted[1] = unsorted[1], unsorted[0]
+      return unsorted[1], unsorted[0]
     end
-    
+
     unsorted
   end
 
--- spec/quicksort_spec.rb
+++ spec/quicksort_spec.rb
@@ -12,4 +12,11 @@ describe QuickSort do
     expect(QuickSort.sort([1, 2])).to eq([1, 2])
     expect(QuickSort.sort([2, 1])).to eq([1, 2])
   end
+
+  it 'does not modify original array' do
+    unsorted = [2, 1]
+
+    expect(QuickSort.sort(unsorted)).to eq([1, 2])
+    expect(unsorted).to eq([2, 1])
+  end
 end
