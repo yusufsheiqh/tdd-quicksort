9af623ec8f296c2f760dbdc7d606062ae127a801
Yusuf <yusuf@madetech.com>
2019-03-29 15:07:12 +0000

    Passing third test

--- lib/quicksort.rb
+++ lib/quicksort.rb
@@ -2,6 +2,10 @@
 
 class QuickSort
   def self.sort(unsorted)
+    if unsorted.length > 1 && unsorted[0] > unsorted[1]
+      unsorted[0], unsorted[1] = unsorted[1], unsorted[0]
+    end
+    
     unsorted
   end
 
--- spec/quicksort_spec.rb
+++ spec/quicksort_spec.rb
@@ -8,5 +8,8 @@ describe QuickSort do
   it 'sorts' do
     expect(QuickSort.sort(empty)).to eq(empty)
     expect(QuickSort.sort([1])).to eq([1])
+
+    expect(QuickSort.sort([1, 2])).to eq([1, 2])
+    expect(QuickSort.sort([2, 1])).to eq([1, 2])
   end
 end
